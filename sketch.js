4f027081f08fa03b219a9bcb5cbce50085e10fa4
simonebinone <57355065+simonebinone@users.noreply.github.com>
2019-11-18 14:44:39 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,29 +1,41 @@
 let capture;
 let myFrame; 
+let croppedFrame;
+let painting; 
 
 function setup() {
 createCanvas(375, 812);
 capture = createCapture(VIDEO);
 //capture.size(375,812);    
 capture.hide();
-myFrame  = loadImage(''); 
+myFrame  = createImage(320, 480)
+croppedFrame = createImage(320, 480)
+painting = loadImage ('KMS263.jpg');
 }
 
 function draw() {
   image(capture, 0, 0, width, width * capture.height / capture.width);
-  if (myFrame == true){
-    image(myFrame, 0, 480, width, width * myFrame.height / myFrame.width);
-  } 
+  if (myFrame){
+    image(painting, 0, 480, width, width * myFrame.height / myFrame.width)
+    image(croppedFrame, 0, 400, width, width * myFrame.height / myFrame.width);
+    
+  }
   
 } 
-
-function captureImage() {
-  myFrame.copy(capture, capture.x, capture.y, capture.height, capture.width,capture.x, capture.y, capture.height, capture.width)  
-  console.log(myFrame)
-}
 
 function keyPressed() {
   if (key == 'x') {
     captureImage()
   }
+}
+
+function captureImage() {
+  myFrame.copy(capture, capture.x, capture.y, capture.height, capture.width,capture.x, capture.y, capture.height, capture.width)  
+  console.log(myFrame)
+  cropFrame()
+}
+
+function cropFrame (){
+  croppedFrame.copy(myFrame, 100, 100, myFrame.height-100, myFrame.width-100, 0, 0, myFrame.height-100, myFrame.width-100,)
+  console.log(croppedFrame)
 }
